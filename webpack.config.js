701b24ca8996670e182ce93292150cd5128f4a9a
rf222cz <rf222cz@student.lnu.se>
2016-04-05 16:40:23 +0200

    new packages and webpack-config
    packages that transform JSX and ES6 to javascript with webpack to a
    minified version

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,19 @@ module.exports = {
   context: __dirname,
   devtool: debug ? "inline-sourcemap" : null,
   entry: "./TestApplication/app.js",
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html.attrs', 'transform-class-properties', 'transform-decorators-legacy']
+        }
+      }
+    ]
+  }
   output: {
     path: __dirname + "/js",
     filename: "scripts.min.js"
