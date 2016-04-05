ffa4b40a28ea7b51529b00657c39ca34e4513080
rf222cz <rf222cz@student.lnu.se>
2016-04-05 16:53:45 +0200

    Testapplication

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   context: __dirname,
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./TestApplication/app.js",
+  entry: "./TestApplication/script.js",
   module: {
     loaders: [
       {
@@ -12,15 +12,15 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html.attrs', 'transform-class-properties', 'transform-decorators-legacy']
+          presets: ['react', 'es2015'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
         }
       }
     ]
-  }
+  },
   output: {
-    path: __dirname + "/js",
-    filename: "scripts.min.js"
+    path: __dirname + "/TestApplication",
+    filename: "script.min.js"
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
