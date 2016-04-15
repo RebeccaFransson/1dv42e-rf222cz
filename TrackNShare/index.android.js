694b0509092b15da86c3e7ebbdd01c2376cdabac
rf222cz <rf222cz@student.lnu.se>
2016-04-15 10:33:47 +0200

    It went to hell..
    read more:
    https://github.com/RebeccaFransson/1dv42e-rf222cz/wiki/Iterationsplaner-Elaboration

--- TrackNShare/index.android.js
+++ TrackNShare/index.android.js
@@ -16,7 +16,7 @@ class TrackNShare extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!!!!!!!
+          Welcome to React Native!!!!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
